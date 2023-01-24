ad4fd8a17ad3d993d7ca8ceef94b9a7859acf09d
Mohammed Huzair Pasha <89265545+LexLuthorHuzair@users.noreply.github.com>
2023-01-24 12:15:12 +0530

    Add files via upload

---
+++ day_2.java
@@ -0,0 +1,29 @@
+package pack1;
+
+public class day_2 {
+    public static void main(String [] args){
+
+//    primitive data types
+
+        int a = 10;
+        float b = 5.55f;
+        double c  = 5.555;
+        char d = 'f';
+        String s = " this is a string";
+        boolean bd = true;
+
+//        Arithmetic expressions
+        int x  = 34;
+        int y = 45;
+        System.out.println("addition" + x+y);
+        int sub = x-y;
+        System.out.println("sub"+ sub);
+        System.out.println("divide" + x/y);
+        System.out.println("modulus" + x%y);
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x--);
+        System.out.println(--x);
+
+    }
+}
